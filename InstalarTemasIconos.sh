dba3e9cef9753b284aad103be7dba4eb2c95363e
handark <handark@gmail.com>
2013-10-22 16:04:12 -0500

    Optimizando el script

--- InstalarTemasIconos.sh
+++ InstalarTemasIconos.sh
@@ -12,54 +12,14 @@ sudo add-apt-repository ppa:noobslab/nitrux-os
 sudo add-apt-repository ppa:satyajit-happy/themes
 sudo apt-get update
 
-echo "Instalando Iconos Trevilla"
-sudo apt-get install trevilla-icons
-sudo apt-get install trevilla-themes
+echo "Instalando Iconos:"
+sudo apt-get -y install trevilla-icons elementary-icons zoncolor-icons potenza potenza-orange awoken-icon-theme fs-icons nouvegnome-gray nouvegnome-color aery-icons malys-ex royal-linux-icons inx-icons nitrux-icons2
 
-echo "Instalando Iconos de Elementary"
-sudo apt-get install elementary-icons
+echo "Instalando Temas:"
+sudo apt-get -y install trevilla-themes royal-linux-themes orion-gtk-theme elementaryish numix-theme zen-suite ambiance-lime nokto-theme
 
-echo "Instalando Temas e Iconos Zoncolor"
-sudo apt-get install zoncolor-icons
-sudo apt-get install zoncolor-themes
 
-echo "Instalando Temas e Iconos Potenza"
-sudo apt-get install potenza
-sudo apt-get install potenza-orange
 
-echo "Instalando Temas e iconos Awoken"
-sudo apt-get install awoken-icon-theme
-
-echo "Instalando Temas e iconos FS"
-sudo apt-get install fs-icons
-
-echo "Instalando Temas e iconos nouvegnome"
-sudo apt-get install nouvegnome-gray
-sudo apt-get install nouvegnome-color
-
-echo "Instalando Temas e iconos aery"
-sudo apt-get install aery-icons
-
-echo "Instalando Temas e iconos malys-ex"
-sudo apt-get install malys-ex
-
-echo "Instalando Temas e iconos royal-linux"
-sudo apt-get install royal-linux-icons
-sudo apt-get install royal-linux-themes
-
-echo "Instalando Temas e iconos nitrux"
-sudo apt-get install inx-icons
-sudo apt-get install nitrux-icons2
-
-echo "Instalando Tema orion"
-sudo apt-get install orion-gtk-theme
-
-echo "Instalando el resto, me mame de escribir"
-sudo apt-get install elementaryish
-sudo apt-get install numix-theme
-sudo apt-get install zen-suite
-sudo apt-get install ambiance-lime
-sudo apt-get install nokto-theme
 sudo apt-get install gnomishgray
 sudo apt-get install gnome-shell-theme-elegance-colors
 sudo apt-get install eos-gs
